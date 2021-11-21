a77ede653c20e819d78f48dd4b234de3d254e93f
shfkd123 <shfkd123@gmail.com>
2021-11-21 20:53:34 +0900

    TDD&UnitTest

---
+++ src/test/java/com/project/app/dto/HelloResponseDtoTest.java
@@ -0,0 +1,22 @@
+package com.project.app.dto;
+
+import com.project.app.web.dto.HelloResponseDto;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lomBook_Test() {
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name,amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
